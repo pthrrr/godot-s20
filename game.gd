c6e6ee27034a977ee07acd30232b335422e0042e
pthrrr <29637548+pthrrr@users.noreply.github.com>
2026-06-05 17:57:00 +0200

    cleanup

--- game.gd
+++ game.gd
@@ -1,28 +1,5 @@
 extends Node2D
 
-#@onready var viewport_1: SubViewport = $HBoxContainer/SubViewportContainer/SubViewport
-#@onready var viewport_2: SubViewport = $HBoxContainer/SubViewportContainer2/SubViewport
-#@onready var follow_camera: Camera3D = $HBoxContainer/SubViewportContainer2/SubViewport/Camera3D
-#
-#
-#func _ready():
-	## Share the 3D world between both viewports
-	#viewport_2.world_3d = viewport_1.world_3d
-#
-	## Get player references from the level
-	#var level = viewport_1.get_node("Level1")
-	#var player_1 = level.get_node("Player")
-	#var player_2 = level.get_node("Player2")
-#
-	## Set Player1's camera as current in viewport 1
-	#player_1.get_node("Camera3D").current = true
-	## Disable Player2's camera in viewport 1
-	#player_2.get_node("Camera3D").current = false
-#
-	## Set up the follow camera to track Player2's camera
-	#follow_camera.current = true
-	#follow_camera.target_camera = player_2.get_node("Camera3D")
-
 @onready var players := {
 	"1": {
 		viewport = $"HBoxContainer/SubViewportContainer/SubViewport",
--- player/follow_camera.gd
+++
@@ -1,8 +0,0 @@
-extends Camera3D
-
-var target_camera: Camera3D
-
-
-func _process(_delta):
-	if target_camera:
-		global_transform = target_camera.global_transform
