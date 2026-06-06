372d3350f65ed9f4c6352c984fdf696aae4db31b
pthrrr <29637548+pthrrr@users.noreply.github.com>
2026-06-06 23:15:31 +0200

    step 14: setting up the kill plane

--- game.gd
+++ game.gd
@@ -1,5 +1,8 @@
 extends Node2D
 
+var spawn_position_player: Vector3
+var spawn_position_player2: Vector3
+
 @onready var players := {
 	"1": {
 		viewport = $"HBoxContainer/SubViewportContainer/SubViewport",
@@ -14,8 +17,21 @@ extends Node2D
 }
 
 func _ready():
+	spawn_position_player = players["1"].player.global_position
+	spawn_position_player2 = players["2"].player.global_position
 	players["2"].viewport.world_3d = players["1"].viewport.world_3d
 	for node in players.values():
 		var remote_transform := RemoteTransform3D.new()
 		remote_transform.remote_path = node.camera.get_path()
 		node.player.get_node("Camera3D").add_child(remote_transform)
+
+
+func _on_kill_plane_body_entered(body):
+	if not body is CharacterBody3D:
+		return
+	# get_tree().reload_current_scene.call_deferred()
+	if body.controls.player_index == 0:
+		body.global_position = spawn_position_player
+	elif body.controls.player_index == 1:
+		body.global_position = spawn_position_player2
+	body.velocity = Vector3.ZERO
