9cf56c913c453a5046c8a5a36af30e7b2b4fa478
pthrrr <29637548+pthrrr@users.noreply.github.com>
2026-06-09 11:27:47 +0200

    add exit button

--- main_screen.gd
+++ main_screen.gd
@@ -1,7 +1,11 @@
 extends Control
 
 
-func _on_button_pressed() -> void:
+func _on_exit_button_pressed() -> void:
+	get_tree().quit()
+
+
+func _on_restart_button_pressed() -> void:
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 	get_tree().paused = false
 	get_tree().reload_current_scene()
