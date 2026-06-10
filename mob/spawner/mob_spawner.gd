c3934b181e435b70fe6defa98603be56cae7e19a
pthrrr <29637548+pthrrr@users.noreply.github.com>
2026-06-10 13:54:59 +0200

    add circular movement to spawners orbiting around platform center

--- mob/spawner/mob_spawner.gd
+++ mob/spawner/mob_spawner.gd
@@ -3,11 +3,25 @@ extends Node3D
 signal mob_spawned(mob)
 
 var mob_counter = 0
+var angle = 0.0
+var radius = 20
+var speed = 0.5
+var center = Vector3.ZERO
+
 
 @export var mob_to_spawn: PackedScene = null
 
 @onready var spawn_marker = %SpawnMarker
 
+
+func _ready():
+	angle = atan2(global_position.z, global_position.x)
+
+func _physics_process(delta):
+	angle += speed * delta
+	global_position.x = center.x + cos(angle) * radius
+	global_position.z = center.z + sin(angle) * radius
+	
 
 func _on_timer_timeout():
 	var new_mob = mob_to_spawn.instantiate()
